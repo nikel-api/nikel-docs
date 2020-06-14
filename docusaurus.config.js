d478a1e8c35c83f0787b0806c3a4dbc5b8325bae
Daren Liang <daren@darenliang.com>
2020-06-14 10:39:11 -0400

    Update docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,83 +1,88 @@
 module.exports = {
-  title: 'Nikel API',
-  tagline: 'Free Unlimited API for the University of Toronto',
-  url: 'https://nikei-api.github.io',
-  baseUrl: '/',
-  favicon: 'img/nikel-api.ico',
-  organizationName: 'nikel-api', // Usually your GitHub org/user name.
-  projectName: 'nikel', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'Nikel API',
-      logo: {
-        alt: 'Nikel API Logo',
-        src: 'img/nikel-api.ico',
-      },
-      links: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+    title: 'Nikel API',
+    tagline: 'Free Unlimited API for the University of Toronto',
+    url: 'https://nikei-api.github.io',
+    baseUrl: '/',
+    favicon: 'img/nikel-api.ico',
+    organizationName: 'nikel-api', // Usually your GitHub org/user name.
+    projectName: 'nikel', // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: 'Nikel API',
+            logo: {
+                alt: 'Nikel API Logo',
+                src: 'img/nikel-api.ico',
+            },
+            links: [
+                {
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'Docs',
+                    position: 'left',
+                },
+                // {to: 'blog', label: 'Blog', position: 'left'},
+                {
+                    href: 'https://github.com/nikel-api',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+                {
+                    href: 'https://status.nikel.ml',
+                    label: 'API Status',
+                    position: 'right',
+                },
+            ],
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/nikel-api',
-          label: 'GitHub',
-          position: 'right',
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Official Documentation',
+                            to: 'docs/',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/nikel-api',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: 'blog',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} nikel-api.`,
         },
-      ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
+    presets: [
+        [
+            '@docusaurus/preset-classic',
             {
-              label: 'Official Documentation',
-              to: 'docs/',
+                docs: {
+                    // It is recommended to set document id as docs home page (`docs/` path).
+                    homePageId: 'welcome',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                },
+                blog: {
+                    showReadingTime: true,
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
             },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/nikel-api',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} nikel-api.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'welcome',
-          sidebarPath: require.resolve('./sidebars.js'),
-        },
-        blog: {
-          showReadingTime: true,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+        ],
     ],
-  ],
 };
--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,6 @@ module.exports = {
     someSidebar: {
         "Getting Started": ['welcome', 'query_guide'],
         Courses: ['courses_schema', 'courses', 'courses_id', 'courses_search'],
-        Buildings: ['buildings_schema', 'buildings', 'buildings_id', 'buildings_search'],
-        Status: ['status']
+        Buildings: ['buildings_schema', 'buildings', 'buildings_id', 'buildings_search']
     },
 };
